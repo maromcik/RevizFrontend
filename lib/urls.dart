eab90e4162780b5c1cc5464c606232d6f6219dee
roman <roman.mariancik@gmail.com>
2023-02-06 01:00:58 +0100

    after statnice

--- lib/urls.dart
+++ lib/urls.dart
@@ -1,33 +1,34 @@
-String ip = "192.168.1.12";
+String ip = "192.168.1.15";
+String port = "";
 
 Uri getUrl() {
-  return Uri.http("$ip:8000", "/audit/get_devices/");
+  return Uri.http("$ip:$port", "/audit/get_devices/");
 }
 
 Uri getDeviceUrl(String qr) {
-  return Uri.http("$ip:8000", "/audit/$qr/get/");
+  return Uri.http("$ip:$port", "/audit/$qr/get/");
 }
 
 Uri getDevicesByFacilityUrl(String facilityName) {
-  return Uri.http("$ip:8000", "/audit/get_devices_by_facility/$facilityName/");
+  return Uri.http("$ip:$port", "/audit/get_devices_by_facility/$facilityName/");
 }
 
 Uri deleteUrl(String qr) {
-  return Uri.http("$ip:8000", "/audit/$qr/delete/");
+  return Uri.http("$ip:$port", "/audit/$qr/delete/");
 }
 
 Uri updateUrl(String qr) {
-  return Uri.http("$ip:8000", "/audit/$qr/update/");
+  return Uri.http("$ip:$port", "/audit/$qr/update/");
 }
 
 Uri createUrl() {
-  return Uri.http("$ip:8000", "/audit/create_device/");
+  return Uri.http("$ip:$port", "/audit/create_device/");
 }
 
 getFacilitiesUrl() {
-  return Uri.http("$ip:8000", "/audit/get_facilities/");
+  return Uri.http("$ip:$port", "/audit/get_facilities/");
 }
 
 getFacilitiesByNameUrl(facilityName) {
-  return Uri.http("$ip:8000", "/audit/get_facility_by_name/$facilityName/");
+  return Uri.http("$ip:$port", "/audit/get_facility_by_name/$facilityName/");
 }
